86cffb0570621cd62d1aa512df225cfee63f0cdd
Charlieatinstinct <102666182+Charlieatinstinct@users.noreply.github.com>
2024-11-03 09:11:46 +0800

    Initial Commit

---
+++ SupremeSDK/SupremeSDK.h
@@ -0,0 +1,18 @@
+//
+//  SupremeSDK.h
+//  SupremeSDK
+//
+//  Created by Charlie on 2024/11/3.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SupremeSDK.
+FOUNDATION_EXPORT double SupremeSDKVersionNumber;
+
+//! Project version string for SupremeSDK.
+FOUNDATION_EXPORT const unsigned char SupremeSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SupremeSDK/PublicHeader.h>
+
+
